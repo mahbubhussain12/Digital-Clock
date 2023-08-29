68f9093479b8e435c7cd20888d1d1448ed025665
mahbubhussain12 <you@example.com>
2023-08-30 01:19:26 +0600

    time set

---
+++ javascript/index.js
@@ -0,0 +1,6 @@
+let hrs = document.getElementById('Hrs')
+let min = document.getElementById('Min')
+let sec = document.getElementById('Sec')
+
+let  currentTime = new Date();
+console.log(currentTime.get);
